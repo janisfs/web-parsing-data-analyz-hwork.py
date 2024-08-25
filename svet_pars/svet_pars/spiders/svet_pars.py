96d0c77fd314f5ca07dab00d8839679f053063ea
janisfs <janisfs@mail.ru>
2024-08-25 19:47:03 +0300

    Update svet_pars.py

--- svet_pars/svet_pars/spiders/svet_pars.py
+++ svet_pars/svet_pars/spiders/svet_pars.py
@@ -3,15 +3,19 @@ import scrapy
 
 class SvetParsSpider(scrapy.Spider):
     name = "svet_pars"
-    allowed_domains = ["https://www.divan.ru"]
+    allowed_domains = ["divan.ru", "www.divan.ru"]  # Учет обеих форм записи сайта
     start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
-        svets = response.css('div._Ud0k')
+        svets = response.css('div._Ud0k')  # Основной контейнер товаров
         for svet in svets:
             yield {
-                "name": svet.css("div.wYUX2 span::text").get(),
-                "price": svet.css("div.pY3d2 span::text").get(),
-                "url": svet.css("a").attrib["href"]
+                "name": svet.css("div.wYUX2 span::text").get(),  # Название товара
+                "price": svet.css("div.pY3d2 span::text").get(),  # Цена товара
+                "url": response.urljoin(svet.css("a").attrib["href"])  # Ссылка на товар
             }
 
+
+
+# Запуск скрепинга следующей строкой через терминал
+# scrapy crawl svet_pars -o result.csv -t csv
