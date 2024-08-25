511d89cae3a3b01d8e7287fe730c635f1b594efa
janisfs <janisfs@mail.ru>
2024-08-25 18:22:40 +0300

    Update svetnewpars.py

--- svet_pars/svet_pars/spiders/svetnewpars.py
+++ svet_pars/svet_pars/spiders/svetnewpars.py
@@ -4,7 +4,7 @@ import scrapy
 class SvetnewparsSpider(scrapy.Spider):
     name = "svetnewpars"
     allowed_domains = ["divan.ru"]
-    start_urls = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
 
     def parse(self, response):
         pass
